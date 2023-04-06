373b5c3424c88e2fa69a369deb536bf3b81e1753
tycheon18 <joong0818@gmail.com>
2023-04-06 09:20:07 +0900

    기능 쪼개기

---
+++ Main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+class Base
+{
+public:
+	void Move();
+	void Attack();
+	Base();
+};
+
+void Base::Move()
+{
+
+};
+
+void Base::Attack()
+{
+
+};
+
+class Player
+{
+public:
+	int HP;
+	int MP;
+	Base PlayerBase();
+};
+
+
+class Monster
+{
+public:
+	Base MonsterBase();
+};
+
+int main()
+{
+	return 0;
+}
